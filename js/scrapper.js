731c96d9dcf4c170c49c1128fa5c1112a73b7f66
elaruiz <elaruiz95@gmail.com>
2017-08-08 12:56:36 -0400

    First commit

---
+++ js/scrapper.js
@@ -0,0 +1,34 @@
+angular.module('TableApp', ['TableApp.controllers','datatables']);
+  
+  angular.module('TableApp.controllers', [])
+
+  .controller('tableController', ["$scope","$http","DTOptionsBuilder", "DTColumnBuilder",function($scope,$http,DTOptionsBuilder, DTColumnBuilder) {
+  	var url = "http://uda-scrapper.us-east-2.elasticbeanstalk.com/api/scraps/simple";
+    $http({
+            method: 'GET',
+            url: url,
+            headers: { 
+            	'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(function (data) {
+            //console.log('status', status);
+            console.log('data', data.data.data);
+            $scope.scrapperList = data.data.data;
+        });
+	      
+    $scope.vm = {};
+
+    $scope.vm.dtOptions = DTOptionsBuilder.newOptions()
+      .withOption('order', [0, 'asc']);
+  }])
+  .controller("modalController", ["$scope", function($scope) {
+    $scope.selRow = function(row) {
+      $scope.selected_row = row;
+      angular.element('#myModal').modal();
+    }
+    $scope.isSelected = function(row) {
+      return $scope.selected_ === row;
+    }
+
+  }
+]);
